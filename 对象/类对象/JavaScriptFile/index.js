32e6eeed1fa4b816af51a0bc31fcf4652bf9d684
liupeng <LIUPENG.0@outlook.com>
2018-12-17 15:59:15 +0800

    类对象 引用外部JavaScript代码

---
+++ 对象/类对象/JavaScriptFile/index.js
@@ -0,0 +1,9 @@
+var person =
+{
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "bule"
+};
+
+document.getElementById("dome").innerHTML = person.firstName + "·" + person.lastName + " is " + person.age + " years ord!";
